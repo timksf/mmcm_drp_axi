671b3351205a26191d61d16a314c098636c3bdff
Tim Köhler <33375734+timksf@users.noreply.github.com>
2025-05-17 21:57:25 +0200

    initial commit

---
+++ nix/flake.nix
@@ -0,0 +1,25 @@
+{
+    description = "";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/24.11";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = { self, flake-utils, nixpkgs } :
+        flake-utils.lib.eachSystem ["x86_64-linux"] (system: let
+            pkgs = import nixpkgs {
+                inherit system;
+            };
+            in { 
+                devShell = with pkgs; pkgs.mkShellNoCC {
+                    packages = with pkgs; [
+                        gcc13
+                        cmake
+                        bluespec
+                        yosys
+                    ];
+                };
+            }
+        );
+}
